c563383675302c6da63adb15d6ef816b3893739c
romarioshir <roma-roman26@yandex.ru>
2022-09-29 23:34:45 +0300

    Two Freinds

---
+++ TwoFreindsAndDog/Program.cs
@@ -0,0 +1,25 @@
+﻿int count = 0;
+int distance = 10000;
+int firstFS = 1;
+int secondFS = 2;
+int dogS = 5;
+int freind = 2;
+int time = 0;
+
+while (distance > 10)
+{
+    if (freind == 1)
+    {
+        time = distance / (firstFS + dogS);
+        freind = 2;
+    }
+    time = distance / (secondFS + dogS);
+    freind = 1;
+
+    distance = distance - (firstFS + secondFS) * time;
+
+    count++;
+}
+Console.Write("Собака пробежит ");
+Console.Write(count);
+Console.WriteLine(" раз");
